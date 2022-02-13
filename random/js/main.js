71ac2a6a19cfbeb8da994f17c10454511b0519d3
Dimon22104 <dima-moskalev-98@mail.ru>
2022-02-14 01:23:20 +0300

    feat: js function

--- random/js/main.js
+++ random/js/main.js
@@ -1,33 +1,18 @@
-const imeg = document.querySelector('.width-imeg__imeg');
 const button = document.querySelector('.wrapper__btn');
-const text = document.querySelector('.wrapper__paragraf');
-// const urlImeg = "http://aws.random.cat/meow";
-const urlQuote = "https://favqs.com/api/qotd";
-
-// async function fetchGet() {
-//     try {
-//         const response = await fetch(urlImeg);
-//         const data = await response.json();
-//         imeg.src = data.file
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// button.addEventListener('click', () => {
-//     fetchGet();
-// });
+const imeg = document.querySelector('.width-imeg__imeg');
+const url = "https://favqs.com/api/qotd";
+const urlImeg = "http://aws.random.cat/meow";
 
 async function randomQuote() {
-    try {
-        const response = await fetch(urlQuote);
-        const data = await response.json();
-        text.innerHTML = data.file
-        console.log(text.innerHTML = data);
-        // console.log(response);
-    } catch (error) {
-        console.log(error);
-    }
+    // Quote
+    const resFirst = await fetch(url);
+    const dataFirst = await resFirst.json();
+    let paragraph = document.querySelector('p');
+    paragraph.textContent = dataFirst.quote.body;
+    // Imeg
+    const resSecond = await fetch(urlImeg);
+    const dataSecond = await resSecond.json();
+    imeg.src = dataSecond.file;
 }
 
 button.addEventListener('click', () => {
